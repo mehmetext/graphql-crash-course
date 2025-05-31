a4faa3214f8e29d90b01b6cfbf67ecc315522ef0
Mehmet <mehmetext68@gmail.com>
2025-05-31 14:51:10 +0300

    feat: add resolvers example

--- src/graphql/apollo-server.ts
+++ src/graphql/apollo-server.ts
@@ -1,11 +1,13 @@
 import { ApolloServer } from "@apollo/server";
 import { readFileSync } from "fs";
 import path from "path";
+import resolvers from "./resolvers";
 
 const typeDefs = readFileSync(path.join(__dirname, "schema.graphql"), "utf-8");
 
 const apolloServer = new ApolloServer({
   typeDefs,
+  resolvers,
 });
 
 export default apolloServer;
